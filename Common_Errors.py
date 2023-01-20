4c821302732d3bde74813681357b7a94fbdcfdf1
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-20 19:05:13 +0530

    Add files via upload

---
+++ Common_Errors.py
@@ -0,0 +1,15 @@
+"""
+Day: Friday
+Date: 20th Jan 2023
+Time: 6:30
+Topic: Common Errors
+Author: Areeb Ahmed
+"""
+
+# print(3/0) # ZeroDivisionerror
+# print(hh) # NameError
+# a="hello"
+# print(int(a)) #valueError 
+# b=4.5
+# print(b+"Hello") # TypeError
+# print(['hello'].lower()) #AttributeError
